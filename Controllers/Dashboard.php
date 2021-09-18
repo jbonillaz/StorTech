a29e43713e89a4523e5ccd6fb1888662752f3b59
jbonillaz <damilan1991@gmail.com>
2021-09-17 20:54:46 -0500

    Proyecto tienda virtual, hasta 88. Extraer modulos y  Roles de Usuario.

---
+++ Controllers/Dashboard.php
@@ -0,0 +1,28 @@
+<?php 
+
+	class Dashboard extends Controllers{
+
+		public function __construct(){
+
+			parent::__construct();
+
+			session_start();
+			// session_regenerate_id(true);
+			if(empty($_SESSION['login'])){
+				
+				header('Location: '.base_url().'/login');
+			}
+			// getPermisos(1);
+		}
+
+		public function dashboard()
+		{
+			$data['page_id'] = 2;
+			$data['page_tag'] = "Dasboard -Tienda Virtual";
+			$data['page_title'] = "dasboard -tienda Virtual";
+			$data['page_name'] = "dashboard";
+			$this->views->getView($this,"dashboard",$data);
+		}
+
+	}
+ ?>
---
+++ Libraries/Core/Conexion.php
@@ -0,0 +1,22 @@
+<?php
+class Conexion{
+	private $conect;
+
+	public function __construct(){
+		$connectionString = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=".DB_CHARSET;
+		try{
+			$this->conect = new PDO($connectionString, DB_USER, DB_PASSWORD);
+			$this->conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		    //echo "conexi¨®n exitosa";
+		}catch(PDOException $e){
+			$this->conect = 'Error de conexi¨®n';
+		    echo "ERROR: " . $e->getMessage();
+		}
+	}
+
+	public function conect(){
+		return $this->conect;
+	}
+}
+
+?>
