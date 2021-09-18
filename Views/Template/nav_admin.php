cae4092e004b9d489b94f72d0d5e83a29ef35ffa
jbonillaz <damilan1991@gmail.com>
2021-09-17 21:32:02 -0500

    89. Configuracion del acceso al menu de acuerdo a los roles asignados por el administrador. S/N."

--- Views/Template/nav_admin.php
+++ Views/Template/nav_admin.php
@@ -8,50 +8,72 @@
         </div>
       </div>
       <ul class="app-menu">
-        <li>
-          <a class="app-menu__item" href="<?= base_url(); ?>/dashboard">
-            <i class="app-menu__icon fa fa-dashboard"></i>
-            <!-- <i class="app-menu__icon fas fa-tachometer-alt-slow"></i> -->
-            <span class="app-menu__label">Dashboard</span>
-          </a>
-        </li>
-        <li class="treeview">
-          <a class="app-menu__item" href="#" data-toggle="treeview">
-          <i class="app-menu__icon fas fa-users"></i>
-          
-            <!-- <i class="app-menu__icon fa fa-users" aria-hidden="true"></i> -->
-            <span class="app-menu__label">Usuarios</span>
-            <i class="treeview-indicator fa fa-angle-right"></i>
-          </a>
-          <ul class="treeview-menu">
-            <li><a class="treeview-item" href="<?= base_url(); ?>/usuarios"><i class="icon fa fa-circle-o"></i> Usuarios</a></li>
-            <li><a class="treeview-item" href="<?= base_url(); ?>/roles" rel="noopener"><i class="icon fa fa-circle-o"></i> Roles</a></li>
-            <li><a class="treeview-item" href="<?= base_url(); ?>/permisos"><i class="icon fa fa-circle-o"></i> Permisos</a></li>
-          </ul>
-        </li>
-        
-        <li>
-          <a class="app-menu__item" href="<?= base_url(); ?>/clientes">
-            <i class="app-menu__icon fas fa-user"></i>
-            <span class="app-menu__label">Clientes</span>
-          </a>
-        </li>
-
-        <li>
-          <a class="app-menu__item" href="<?= base_url(); ?>/productos">
-            <i class="app-menu__icon fas fa-archive"></i>
-            <span class="app-menu__label">Productos</span>
-          </a>
-        </li>
-
-        <li>
-          <a class="app-menu__item" href="<?= base_url(); ?>/pedidos">
-            <!-- <i class="app-menu__icon fa fa-shopping-cart" aria-hidden="true"></i> -->
-            <i class="app-menu__icon fas fa-shopping-cart"></i>
-            <span class="app-menu__label">Pedidos</span>
-          </a>
-        </li>
-
+        <!-- Modulo de Dashboard -->
+        <?php if(!empty($_SESSION['permisos'][1]['r'])){ ?>
+          <li>
+            <a class="app-menu__item" href="<?= base_url(); ?>/dashboard">
+              <i class="app-menu__icon fa fa-dashboard"></i>
+              <!-- <i class="app-menu__icon fas fa-tachometer-alt-slow"></i> -->
+              <span class="app-menu__label">Dashboard</span>
+            </a>
+          </li>
+        <?php } ?>
+        <!-- MOdulo de Usuarios -->
+        <?php if(!empty($_SESSION['permisos'][2]['r'])){ ?>
+          <li class="treeview">
+            <a class="app-menu__item" href="#" data-toggle="treeview">
+            <i class="app-menu__icon fas fa-users"></i>
+            
+              <!-- <i class="app-menu__icon fa fa-users" aria-hidden="true"></i> -->
+              <span class="app-menu__label">Usuarios</span>
+              <i class="treeview-indicator fa fa-angle-right"></i>
+            </a>
+            <ul class="treeview-menu">
+              <li><a class="treeview-item" href="<?= base_url(); ?>/usuarios"><i class="icon fa fa-circle-o"></i> Usuarios</a></li>
+              <li><a class="treeview-item" href="<?= base_url(); ?>/roles" rel="noopener"><i class="icon fa fa-circle-o"></i> Roles</a></li>
+            </ul>
+          </li>
+        <?php } ?>
+        <!-- Modulo de Clientes -->
+        <?php if(!empty($_SESSION['permisos'][3]['r'])){ ?>
+          <li>
+            <a class="app-menu__item" href="<?= base_url(); ?>/clientes">
+              <i class="app-menu__icon fas fa-user"></i>
+              <span class="app-menu__label">Clientes</span>
+            </a>
+          </li>
+        <?php } ?>
+        <!-- Modulo de Tienda -->
+        <?php if(!empty($_SESSION['permisos'][4]['r']) || !empty($_SESSION['permisos'][6]['r'])){ ?>
+          <li class="treeview">
+              <a class="app-menu__item" href="#" data-toggle="treeview">
+                  <i class="app-menu__icon fa fa-archive" aria-hidden="true"></i>
+                  <span class="app-menu__label">Tienda</span>
+                  <i class="treeview-indicator fa fa-angle-right"></i>
+              </a>
+              <ul class="treeview-menu">
+                  <!-- Modulo de Categorias -->
+                  <?php if(!empty($_SESSION['permisos'][6]['r'])){ ?>
+                    <li><a class="treeview-item" href="<?= base_url(); ?>/categorias"><i class="icon fa fa-circle-o"></i> Categorías</a></li>
+                  <?php } ?>
+                  <!-- Modulo de productos -->
+                  <?php if(!empty($_SESSION['permisos'][4]['r'])){ ?>
+                    <li><a class="treeview-item" href="<?= base_url(); ?>/productos"><i class="icon fa fa-circle-o"></i> Productos</a></li>
+                  <?php } ?>
+              </ul>
+          </li>
+        <?php } ?>
+        <!-- MOdulo de pedidos -->
+        <?php if(!empty($_SESSION['permisos'][5]['r'])){ ?>
+          <li>
+            <a class="app-menu__item" href="<?= base_url(); ?>/pedidos">
+              <!-- <i class="app-menu__icon fa fa-shopping-cart" aria-hidden="true"></i> -->
+              <i class="app-menu__icon fas fa-shopping-cart"></i>
+              <span class="app-menu__label">Pedidos</span>
+            </a>
+          </li>
+        <?php } ?>
+        <!-- MOdulo para cerrar sesion -->
         <li>
           <a class="app-menu__item" href="<?= base_url(); ?>/logout">
             <i class="app-menu__icon fas fa-sign-out-alt" ></i>
